adfa100adc1a978dd55ded23cf0c72bcb0fcc13a
khoa <Benjamin.dev98@gmail.com>
2020-09-13 22:21:44 +0700

    update

--- index.js
+++ index.js
@@ -37,19 +37,67 @@ const { log, logTitle } = require("logger");
 // console.log(0 === false);
 
 /* Map | Filter  | Reduce */
-var map = [1, 2, 3, 4, 5].map((n) => {
-  return n * 2;
-});
+// var map = [1, 2, 3, 4, 5].map((n) => {
+//   return n * 2;
+// });
 
-console.log("[].map");
-console.log(map);
+// console.log("[].map");
+// console.log(map);
 
-console.log("[].filter");
+// console.log("[].filter");
 
-let filter = [1, 2, 3, 4, 5].filter((n) => {
-  return n >= 3;
-});
+// let filter = [1, 2, 3, 4, 5].filter((n) => {
+//   return n >= 3;
+// });
 
-console.log(filter);
+// console.log(filter);
 
-console.log("[].reduce");
+// console.log("[].reduce");
+
+// let reduce = [1,2,3,4,5].reduce((accumulator, current) =>{
+//     console.log(accumulator);
+//     return accumulator + current; 
+// })
+// console.log(reduce); 
+
+
+/* Spread Operator Arrays*/
+
+// const arrayOne = ['Maria','Anna','Alex']
+// const arrayTwo = ['Said','IsMail','Aisha']
+
+// const concatArray = [...arrayOne];
+
+// concatArray.forEach(name=>{
+//     console.log(name);
+// })
+
+// const name = 'Marian';
+// const nameToArray = [...name];
+// nameToArray.forEach(letters => {
+//     console.log(letters);
+// })
+
+// const addNumbers = (n1,n2,n3) => {
+//     return n1 + n2 + n3; 
+// }
+// const numbers = [1,5,9];
+
+// const result = addNumbers(...numbers);
+// console.log(result);
+
+/* Object and Spread Operator */
+
+// const address = {
+//     city: 'La',
+//     country : 'USA',
+//     postcode : 'LA44'
+// };
+// const name  = {
+//     firstName: 'Andy',
+//     lastName: 'Johnes'
+// }
+
+// const person = {...address,...name};
+// console.log(JSON.stringify(person,null,2));
+// console.log(Object.keys(person));
